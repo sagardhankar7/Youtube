9236cae500a25e022aa4fd784237997f83752e5a
Sagar Dhankar <sag9315@gmail.com>
2026-03-15 14:03:59 +0530

    A way of writing multiple list

--- component/ButtonList.js
+++ component/ButtonList.js
@@ -4,16 +4,10 @@ const ButtonList = () => {
     const buttonCategory = ["All", "Gaming", "Songs", "Cricket", "Live", "LiveCricket"]
     return (
         <div className="flex mt-2.5">
-            {
-                buttonCategory.map((category)=>(
+            {buttonCategory.map((category)=>(
                     <Button key={category} name= {category} />
                 ))
             }
-            <Button name={"All"}/>
-            <Button name={"Gaming"}/>
-            <Button name={"Songs"}/>
-            <Button name={"Cricket"}/>
-            <Button name={"Live"}/>
 
         </div>
     )
