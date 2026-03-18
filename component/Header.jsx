e8255936252d98796c2ede09ffc7a2fe985db6bc
Sagar Dhankar <sag9315@gmail.com>
2026-03-18 10:29:31 +0530

    dummy suggestion box created

--- component/Header.jsx
+++ component/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
 
 
     return (
-        <div className="flex justify-between shadow-lg">
+        <div className="flex justify-between shadow-lg p-2">
             <div className="flex items-center">
                 <div onClick={()=>handleToggleMenu()} className="cursor-pointer">
                     <img className="w-6" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/250px-Hamburger_icon.svg.png"></img>
@@ -42,9 +42,23 @@ const Header = () => {
                 </div>
             </div>
             <div className="flex items-center">
-                <input value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)} className="border" type="text"></input>
+                <div className="relative">
+                    <input value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)} className="border border-gray-200 px-5 py-1 rounded-2xl w-80" type="text"></input>
+                    <div className="absolute my-1 p-2 w-80 rounded-2xl border border-gray-200 bg-white">
+                        <ul>
+                            <li className="hover:bg-gray-200 px-2 rounded cursor-default">🔍Iphone</li>
+                            <li className="hover:bg-gray-200 px-2 rounded cursor-default">🔍Iphone pro</li>
+                            <li className="hover:bg-gray-200 px-2 rounded cursor-default">🔍Iphone pro</li>
+                            <li className="hover:bg-gray-200 px-2 rounded cursor-default">🔍Iphone Pro max</li>
+                            <li className="hover:bg-gray-200 px-2 rounded cursor-default">🔍Iphone Pro max</li>
+                    
+                        </ul>
+                    </div>
+                </div>
                 <button><img className="w-10" src="https://static.vecteezy.com/system/resources/thumbnails/020/120/837/small/search-icon-simple-style-web-searching-background-symbol-search-brand-logo-design-element-search-t-shirt-printing-for-sticker-free-vector.jpg"></img></button>
             </div>
+            
+            
             <div>
                 <img className="w-10" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSLU5_eUUGBfxfxRd4IquPiEwLbt4E_6RYMw&s"></img>
             </div>
