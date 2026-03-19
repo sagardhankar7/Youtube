e90b519b7eaa12d6dbc985b89713a71cf19bc0bb
Sagar Dhankar <sag9315@gmail.com>
2026-03-19 14:28:07 +0530

    API Polling Logic (Long Polling)

--- component/ChatContainer.jsx
+++ component/ChatContainer.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
-import ChatItems from './ChatItems'
+import React, { useEffect } from 'react'
+import ChatMessage from './ChatMessage'
 
 export const ChatContainer = () => {
+    useEffect(()=>{
+        const it = setInterval(()=>{
+            console.log("API Called")
+        },2000)
+
+        return ()=>clearInterval(it)
+    })
+
   return (
-    <div className='border border-black w-full h-[570px] rounded-lg bg-gray-100'><ChatItems name="Sagar" message="This is a sample message." /></div>
+    <div className='border border-black w-full h-[570px] rounded-lg bg-gray-100'>
+        <ChatMessage name="Sagar" message="This is a sample message." />
+
+
+        <ChatMessage name="Sagar" message="This is a sample message." />
+        <ChatMessage name="Sagar" message="This is a sample message." />
+        <ChatMessage name="Sagar" message="This is a sample message." />
+        <ChatMessage name="Sagar" message="This is a sample message." />
+    </div>
   )
 }
--- component/ChatMessage.jsx
+++ component/ChatMessage.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 // receiving name and message (in prop)
-const ChatItems = ({name, message}) => {
+const ChatMessage = ({name, message}) => {
   return (
-    <div className='flex items-center'>
+    <div className='flex items-center bg-gray-150 mb-1 shadow-lg'>
         <img className="w-10 rounded-lg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSLU5_eUUGBfxfxRd4IquPiEwLbt4E_6RYMw&s"></img>
         <span className='font-bold px-2'>{name}</span>
         <span>{message}</span>
@@ -11,4 +11,4 @@ const ChatItems = ({name, message}) => {
   )
 }
 
-export default ChatItems
+export default ChatMessage
