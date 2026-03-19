339c0fd1ffd0a9f129b5abc9072e594fe155f07c
Sagar Dhankar <sag9315@gmail.com>
2026-03-19 14:15:38 +0530

    passing to the prop sample

--- component/ChatContainer.jsx
+++ component/ChatContainer.jsx
@@ -3,6 +3,6 @@ import ChatItems from './ChatItems'
 
 export const ChatContainer = () => {
   return (
-    <div className='border border-black w-full h-[570px] rounded-lg bg-gray-100'><ChatItems /></div>
+    <div className='border border-black w-full h-[570px] rounded-lg bg-gray-100'><ChatItems name="Sagar" message="This is a sample message." /></div>
   )
 }
--- component/ChatItems.jsx
+++ component/ChatItems.jsx
@@ -5,8 +5,8 @@ const ChatItems = ({name, message}) => {
   return (
     <div className='flex items-center'>
         <img className="w-10 rounded-lg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSLU5_eUUGBfxfxRd4IquPiEwLbt4E_6RYMw&s"></img>
-        <span className='font-bold px-2'>Name</span>
-        <span>The message is a dummy message.</span>
+        <span className='font-bold px-2'>{name}</span>
+        <span>{message}</span>
     </div>
   )
 }
